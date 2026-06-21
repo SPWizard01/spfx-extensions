0b4dce2a2f6c5f29ef22fed1b4ba63f192b485a7
SPWizard01 <the.mtrix@gmail.com>
2026-06-21 12:41:10 +0300

    package bump

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,18 @@
-const spfxProfile = require('@microsoft/eslint-config-spfx/lib/flat-profiles/default');
+const spfxProfile = require("@microsoft/eslint-config-spfx/lib/flat-profiles/default");
 
 module.exports = [
   ...spfxProfile,
   {
-    files: ['**/*.ts', '**/*.tsx'],
+    files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
       parserOptions: {
         tsconfigRootDir: __dirname,
-        project: './tsconfig.json'
-      }
+        project: "./tsconfig.json",
+      },
     },
     rules: {
       //implicit is fine.
-      "@typescript-eslint/explicit-function-return-type": 0
-    }
-  }
+      "@typescript-eslint/explicit-function-return-type": 0,
+    },
+  },
 ];
